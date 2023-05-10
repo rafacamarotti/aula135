d766b137c6194c681f15ff97eedde6f7d89fa491
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-05-09 22:10:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function setup(){
     video.hide();
 }
 function start(){
-    objectDetector = ml5.objectDetector('cocssd',modelLoaded);
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
     document.getElementById("status").innerHTML = "status: detectando objetos";
 }
 function modelLoaded(){
@@ -30,12 +30,12 @@ function draw(){
     image(video, 0,0,480,380);
         if(status != ""){
             objectDetector.detect(video, gotResult);
-            for(i= 0;i<objects.legth;i++){
+            for(i= 0;i<objects.length;i++){
                 document.getElementById("status").innerHTML= "Status: Objetos detectados";
-                document.getElementById("numberofobjects").innerHTML="quantidade de objetos detectados: "+objects.legth;
+                document.getElementById("numberofobjects").innerHTML="quantidade de objetos detectados: "+objects.length;
                 fill("#FF0000");
                 percent = floor(objects[i].confidence*100);
-                text(objects[i].label+" "+percent+"%",object[i].x+15,objects[i],y+15);
+                text(objects[i].label+" "+percent+"%",objects[i].x+15,objects[i].y+15);
                 noFill();
                 stroke("#FF0000");
                 rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
